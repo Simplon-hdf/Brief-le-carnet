91260d396f42b6dbf38a792014c1e0e7ebc2eb79
Cédric L. <134500848+Muryokukan@users.noreply.github.com>
2025-06-06 14:22:34 +0200

    Revert "Feature/footer"

--- Annuaire-de-contact/src/app/app.ts
+++ Annuaire-de-contact/src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Footer } from './footer/footer';
-import { HeaderComponent } from './header-component/header-component';
-import { Home } from './home/home';
+import { HeaderComponent } from "./header-component/header-component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer, HeaderComponent, Home],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
