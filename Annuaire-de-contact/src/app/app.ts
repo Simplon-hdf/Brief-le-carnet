8dd7ac2e3b0437bd4af1af95a21e909b5bc3c263
Amine.B <amine.benfetta711@gmail.com>
2025-06-06 13:39:19 +0200

    feat(contact-list): include ContactList component in app imports

--- Annuaire-de-contact/src/app/app.ts
+++ Annuaire-de-contact/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContactList } from "./contact-list/contact-list";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContactList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
