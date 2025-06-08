169a6c86ebd06096703c8e0207f9364aac6ed374
Loicwr <nephtyse19@hotmail.fr>
2025-06-08 12:18:32 +0200

    feat(annuaire): set up Annuaire component with header, footer, and user form

--- Annuaire-de-contact/src/app/app.routes.ts
+++ Annuaire-de-contact/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Annuaire } from './pages/annuaire/annuaire';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'annuaire', component:Annuaire}
+];
--- Annuaire-de-contact/src/app/pages/annuaire/annuaire.ts
+++ Annuaire-de-contact/src/app/pages/annuaire/annuaire.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { UserFormComponent } from "../../user-form-component/user-form-component";
+import { HeaderComponent } from "../../header-component/header-component";
+import { FooterComponent } from "../../footer-component/footer-component";
 
 @Component({
   selector: 'app-annuaire',
-  imports: [],
+  imports: [UserFormComponent, HeaderComponent, FooterComponent],
   templateUrl: './annuaire.html',
   styleUrl: './annuaire.css'
 })
