da239b8504554f1f90044fec8b71f1b937e8c584
Loicwr <nephtyse19@hotmail.fr>
2025-06-11 14:47:02 +0200

    feat(card-interimaire): add initial component structure with HTML, CSS, and test files

---
+++ Annuaire-de-contact/src/app/card-interimaire-component/card-interimaire-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-interimaire-component',
+  imports: [],
+  templateUrl: './card-interimaire-component.html',
+  styleUrl: './card-interimaire-component.css'
+})
+export class CardInterimaireComponent {
+
+}
