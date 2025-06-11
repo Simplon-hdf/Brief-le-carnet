f0547989ec29069f969423f58425ae5eb1a8f64e
Loicwr <nephtyse19@hotmail.fr>
2025-06-11 15:40:16 +0200

    feat(card-entreprise-component): integrate CommonModule and add test image; update home page imports

--- Annuaire-de-contact/src/app/card-entreprise-component/card-entreprise-component.ts
+++ Annuaire-de-contact/src/app/card-entreprise-component/card-entreprise-component.ts
@@ -1,11 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card-entreprise-component',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card-entreprise-component.html',
   styleUrl: './card-entreprise-component.css'
 })
 export class CardEntrepriseComponent {
+//test image
+testPng = 'assets/pexels-olly-774909.jpg'; 
 
+  isExpanded = false;
+  toggleExpanded = !this.isExpanded;
+
+donneForm = {
+  type: 'entreprise',
+  nom: 'TechCorp',
+  photo: 'assets/pexels-olly-774909.jpg',
+  telephone: '0145678910',
+  email: 'contact@techcorp.com',
+  adressePostal: '10 avenue de la République',
+  codePostal: '69000',
+  metier: 'Recherche développeurs Angular',
+  description: 'Entreprise innovante dans le domaine des technologies cloud.'
+};
 }
--- Annuaire-de-contact/src/app/pages/home-page/home-page.ts
+++ Annuaire-de-contact/src/app/pages/home-page/home-page.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Annuaire } from '../annuaire/annuaire';
+import { CardEntrepriseComponent } from "../../card-entreprise-component/card-entreprise-component";
+import { CardInterimaireComponent } from "../../card-interimaire-component/card-interimaire-component";
 
 @Component({
   selector: 'app-home-page',
-  imports: [Annuaire],
+  imports: [Annuaire, CardEntrepriseComponent, CardInterimaireComponent],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css'
 })
