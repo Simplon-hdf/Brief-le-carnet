6221e6d50dbf970b7165514777ce132d940362fb
Loicwr <nephtyse19@hotmail.fr>
2025-06-11 19:31:58 +0200

    feat(home-page): remove unused card components from imports for cleaner code

--- Annuaire-de-contact/src/app/pages/home-page/home-page.ts
+++ Annuaire-de-contact/src/app/pages/home-page/home-page.ts
@@ -3,10 +3,11 @@ import { Annuaire } from '../annuaire/annuaire';
 import { CardEntrepriseComponent } from "../../card-entreprise-component/card-entreprise-component";
 import { CardInterimaireComponent } from "../../card-interimaire-component/card-interimaire-component";
 import { CardContainerComponent } from "../../card-container-component/card-container-component";
+import { UserFormComponent } from "../../user-form-component/user-form-component";
 
 @Component({
   selector: 'app-home-page',
-  imports: [Annuaire, CardEntrepriseComponent, CardInterimaireComponent, CardContainerComponent],
+  imports: [Annuaire, CardContainerComponent],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css'
 })
