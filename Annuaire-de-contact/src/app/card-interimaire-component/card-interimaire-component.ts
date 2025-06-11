eb2a8665a3de90aa742617b9a693056bdb07fd9a
Loicwr <nephtyse19@hotmail.fr>
2025-06-11 16:45:01 +0200

    feat(card-interimaire-component): add initial data structure and toggle functionality

--- Annuaire-de-contact/src/app/card-interimaire-component/card-interimaire-component.ts
+++ Annuaire-de-contact/src/app/card-interimaire-component/card-interimaire-component.ts
@@ -7,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './card-interimaire-component.css'
 })
 export class CardInterimaireComponent {
+    donneForm = {
+    nom: 'TechCorp',
+    photo: 'assets/pexels-olly-774909.jpg',
+    email: 'contact@techcorp.com',
+    telephone: '0123456789',
+    adressePostal: '10 rue de Paris',
+    codePostal: '75001',
+    metier: 'Recrutement développeur',
+    description: 'Entreprise innovante dans la tech, spécialisée dans le développement web.'
+  };
 
+  testPng = this.donneForm.photo;
+
+  toggleExpanded() {
+    this.isExpanded = !this.isExpanded;
+  }
 }
