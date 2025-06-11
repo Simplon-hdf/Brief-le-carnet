c0bd1d1bcfb11983ebb59de539d1efefe47313b9
Loicwr <nephtyse19@hotmail.fr>
2025-06-11 17:31:30 +0200

    feat(card-container-component): implement initial structure with CommonModule and sample data

--- Annuaire-de-contact/src/app/card-container-component/card-container-component.ts
+++ Annuaire-de-contact/src/app/card-container-component/card-container-component.ts
@@ -1,11 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card-container-component',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card-container-component.html',
   styleUrl: './card-container-component.css'
 })
 export class CardContainerComponent {
 
+  filtre: 'tous' | 'entreprise' | 'interimaire' = 'tous';
+
+    entreprises = [
+    {
+      nom: 'TechCorp',
+      type: 'entreprise',
+      photo: 'assets/pexels-olly-774909.jpg',
+      email: 'contact@techcorp.com',
+      telephone: '0123456789',
+      adressePostal: '10 rue de Paris',
+      codePostal: '75001',
+      metier: 'Recrutement développeur',
+      description: 'Entreprise innovante dans la tech, spécialisée dans le développement web.'
+    }
+  ];
 }
