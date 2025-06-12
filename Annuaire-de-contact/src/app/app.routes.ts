4093afd6f5b2259d4c813352812b4fd7feafa81e
Muryokukan <c.limosin@outlook.fr>
2025-06-12 12:11:42 +0200

    feat: redirect to HomePage

--- Annuaire-de-contact/src/app/app.routes.ts
+++ Annuaire-de-contact/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { Annuaire } from './pages/annuaire/annuaire';
+import { HomePage } from './pages/home-page/home-page';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/annuaire', pathMatch: 'full' },
-    {path:'annuaire', component:Annuaire}
+   
+    {path:'', component:HomePage}
 ];
